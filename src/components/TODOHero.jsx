82362665f54dba641adeb7aabc9a75d7553ed17f
iamspruce <iamspruce8@gmail.com>
2024-04-07 18:47:26 +0100

    initial commit

---
+++ src/components/TODOHero.jsx
@@ -0,0 +1,15 @@
+function TODOHero({ todos_completed, total_todos }) {
+  return (
+    <section className="todohero_section">
+      <div>
+        <p className="text_large">Task Done</p>
+        <p className="text_small">Keep it up</p>
+      </div>
+      <div>
+        {todos_completed}/{total_todos}
+      </div>
+    </section>
+  );
+}
+
+export default TODOHero;
